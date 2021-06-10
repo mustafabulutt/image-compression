34676318d1e7a5764f4ad8cb289b86a2584a44cb
Mustafa <45540188+mustafabulutt@users.noreply.github.com>
2021-06-10 22:59:21 +0300

    up

---
+++ image-compression/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace image_compression
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.Name = "label3";
+			// 
+			// textBox1
+			// 
+			resources.ApplyResources(this.textBox1, "textBox1");
+			this.textBox1.Name = "textBox1";
+			// 
+			// textBox2
+			// 
+			resources.ApplyResources(this.textBox2, "textBox2");
+			this.textBox2.Name = "textBox2";
+			// 
+			// comboBox1
+			// 
+			resources.ApplyResources(this.comboBox1, "comboBox1");
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Name = "comboBox1";
+			// 
+			// button1
+			// 
+			resources.ApplyResources(this.button1, "button1");
+			this.button1.Name = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			resources.ApplyResources(this.button2, "button2");
+			this.button2.Name = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			resources.ApplyResources(this.button3, "button3");
+			this.button3.Name = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// progressBar1
+			// 
+			resources.ApplyResources(this.progressBar1, "progressBar1");
+			this.progressBar1.Name = "progressBar1";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtResimSec;
+		private System.Windows.Forms.TextBox txtKaydedilecekKonum;
+		private System.Windows.Forms.TextBox txtTargetFolder;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
---
+++ image-compression/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace image_compression
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			for (int i = 10; i <= 100; i = i + 10)
+			{
+				comboBox1.Items.Add(i);
+			}
+			comboBox1.SelectedIndex = 0;
+
+			progressBar1.Visible = false;
+		}
+
+		private void OpenFolderDialog(TextBox Filepath)
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			folderDlg.ShowNewFolderButton = true;
+
+			DialogResult result = folderDlg.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				Filepath.Text = folderDlg.SelectedPath;
+				Environment.SpecialFolder root = folderDlg.RootFolder;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFolderDialog(textBox1);
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFolderDialog(textBox2);
+
+		}
+
+
+
+
+
+
+
+		public static void CompressImage(string SoucePath, string DestPath, int quality)
+		{
+			var FileName = Path.GetFileName(SoucePath);
+			DestPath = DestPath + "\\" + FileName;
+
+			using (Bitmap bmp1 = new Bitmap(SoucePath))
+			{
+				ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+				System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+				EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+				EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
+
+				myEncoderParameters.Param[0] = myEncoderParameter;
+				bmp1.Save(DestPath, jpgEncoder, myEncoderParameters);
+
+			}
+		}
+
+		private static ImageCodecInfo GetEncoder(ImageFormat format)
+		{
+			ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+			foreach (ImageCodecInfo codec in codecs)
+			{
+				if (codec.FormatID == format.Guid)
+				{
+					return codec;
+				}
+			}
+			return null;
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			progressBar1.Visible = true;
+
+			string[] files = Directory.GetFiles(textBox1.Text);
+			int i = 0;
+			foreach (var file in files)
+			{
+				i++;
+				string ext = Path.GetExtension(file).ToUpper();
+				if (ext == ".PNG" || ext == ".JPG"){ 
+					CompressImage(file, textBox2.Text, (int)comboBox1.SelectedItem);
+					if (i <95) {
+					progressBar1.Value = i;
+
+					}
+				}
+
+			}
+			progressBar1.Value = 100;
+
+
+			MessageBox.Show("Resimleri küçülttüm metin abi şu yola kayıt ettim bi bakarsın :D \n" + textBox2.Text);
+			textBox2.Text = "";
+			textBox1.Text = "";
+			progressBar1.Value = 0;
+
+		}
+
+	}
+}
+
+
+
